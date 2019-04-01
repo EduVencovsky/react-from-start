07a47fc5cf5ed7c6b0f932b895201fd4951795d8
Eduardo Vencovsky <eduvencovsky@gmail.com>
2019-04-01 16:06:45 -0300

    Fix dev webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -54,7 +54,7 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            sourceMap: false,
+                            sourceMap: true,
                         },
                     },
                 ],
