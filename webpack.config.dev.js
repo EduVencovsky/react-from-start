84b4514acb08cca4c303650593f73dab8152f822
Eduardo Vencovsky <eduvencovsky@gmail.com>
2019-04-01 15:57:17 -0300

    dev webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    devtool: 'cheap-module-source-map',
+    entry: path.resolve(__dirname, 'src', 'app'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        stats: 'minimal',
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'app', 'index.html'),
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+            },
+            {
+                test: /\.(css|scss)$/,
+                include: path.resolve(__dirname, 'src', 'app'),
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            discardDuplicates: true,
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: '[name]__[local]___[hash:base64:5]',
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg)$/,
+                use: ["file-loader"]
+            }
+        ]
+    }
+}
